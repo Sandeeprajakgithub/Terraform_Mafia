7f0ace6fd6cb6d068deb49dca3bd0e15a9a72ac5
Sandeep Rajak <sandeeprajakjbp2002@gmail.com>
2026-03-30 01:58:04 +0530

    adding day 2 material

---
+++ learningModules/module/ec2/variables.tf
@@ -0,0 +1,13 @@
+variable "ami"{
+    description = "I am going to provide this ami detail in runtime."
+}
+
+
+variable "instance_type"{
+    description = "I am going to provide instance_type in runtime"
+}
+
+
+variable "name"{
+    name="I am going to provide variable_name in runtime"
+}
---
+++ learningModules/outputs.tf
@@ -0,0 +1,7 @@
+output "dev_vm_ki_ip"{
+    value = module.dev_env_vm.instance_ip
+}
+
+output "prod_vm_ki_ip"{
+    value = module.prod_env_vm.instance_ip
+}
